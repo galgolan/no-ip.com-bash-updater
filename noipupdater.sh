347c0b2c4d21a3bd032f43e525fa87c1670cbc32
Gal Golan <gal@salespredict.com>
2014-03-13 13:06:22 +0200

    Update noipupdater.sh
    changed wget to curl to work on OpenElec

--- noipupdater.sh
+++ noipupdater.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-
 # No-IP uses emails as passwords, so make sure that you encode the @ as %40
-USERNAME=username
-PASSWORD=password
-HOST=hostsite
-LOGFILE=logdir/noip.log
-STOREDIPFILE=configdir/current_ip
-USERAGENT="Simple Bash No-IP Updater/0.4 antoniocs@gmail.com"
+USERNAME=username%40gmail.com
+PASSWORD=mypass
+HOST=myhost.no-ip.org
+LOGFILE=~/noip/noip.log
+STOREDIPFILE=~/noip/current_ip
+USERAGENT="Simple Bash No-IP Updater/0.4"
 
 if [ ! -e $STOREDIPFILE ]; then 
 	touch $STOREDIPFILE
 fi
 
-NEWIP=$(wget -O - http://icanhazip.com/ -o /dev/null)
+NEWIP=$(curl http://icanhazip.com/)
 STOREDIP=$(cat $STOREDIPFILE)
 
 if [ "$NEWIP" != "$STOREDIP" ]; then
-	RESULT=$(wget -O "$LOGFILE" -q --user-agent="$USERAGENT" --no-check-certificate "https://$USERNAME:$PASSWORD@dynupdate.no-ip.com/nic/update?hostname=$HOST&myip=$NEWIP")
+	RESULT=$(curl "https://$USERNAME:$PASSWORD@dynupdate.no-ip.com/nic/update?hostname=$HOST&myip=$NEWIP")
 
 	LOGLINE="[$(date +"%Y-%m-%d %H:%M:%S")] $RESULT"
 	echo $NEWIP > $STOREDIPFILE
@@ -25,6 +24,4 @@ else
 fi
 
 echo $LOGLINE >> $LOGFILE
-
 exit 0
-
